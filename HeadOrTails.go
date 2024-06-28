8df9f613638a40cb825597bb632316e0aa9c5076
onion0904 <takerudaze0904@gmain.com>
2024-06-28 10:46:16 +0900

    HoT: initial revision

--- HeadOrTails.go
+++ HeadOrTails.go
@@ -1,21 +1,24 @@
+package main
+
 import (
 	"fmt"
-	"rand"
+	"math/rand"
+	"time"
 )
 
 func main() {
 	fmt.Println("Tossing a coin...")
+	rand.Seed(time.Now().UnixNano())
 	var count1, count2 int
 	for i := 0; i < 3; i++ {
-		R :=rand.Intn(100)
-		if R % 2 == 0 {
-			fmt.Println("Round %d:Heads",i+1)
+		R := rand.Intn(100)
+		if R%2 == 0 {
+			fmt.Printf("Round %d: Heads\n", i+1)
 			count1++
-		}else if R%2 == 1 {
-			fmt.Println("Round %d:Tails",i+1)
+		} else {
+			fmt.Printf("Round %d: Tails\n", i+1)
 			count2++
 		}
 	}
-	fmt.Println("Heads: %d, Tails: %d",count1,count2)
-	return
+	fmt.Printf("Heads: %d, Tails: %d\n", count1, count2)
 }
